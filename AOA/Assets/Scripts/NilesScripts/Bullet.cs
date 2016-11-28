b909ac2990255172c74d30636681fea19f98544f
Reminault <nilemc12@hotmail.com>
2016-11-28 10:25:43 +0400

    Bullet Changes and sound files added

--- AOA/Assets/Scripts/NilesScripts/Bullet.cs
+++ AOA/Assets/Scripts/NilesScripts/Bullet.cs
@@ -5,6 +5,7 @@ public class Bullet : MonoBehaviour {
 
 	public Transform targetPosition;
 	public Vector3 bulletTvlDist;
+	public Renderer bulletRend;
 	Vector3 bltSpeed;
 	public Vector3 bltVelo;
 	public Vector3 firingPosition;
@@ -21,6 +22,8 @@ public class Bullet : MonoBehaviour {
 		activeBullet = false;
 		//bulletDamage = GameObject.Find("Soldier").GetComponent<PlayerOperator> ().getPlayerDamage();
 		int liveBulletCount;
+		bulletRend = GetComponent<Renderer> ();
+		bulletRend.enabled = false;
 	}
 
 	// Update is called once per frame
@@ -36,16 +39,13 @@ public class Bullet : MonoBehaviour {
 	{
 		if (enemy.gameObject.tag == "Enemy") 
 		{
+			bulletRend.enabled = false;
 			this.gameObject.GetComponent<Rigidbody> ().Sleep ();
 			enemy.gameObject.GetComponent<enemyController>().getAndTakeDamage(bulletDamage);
 			activeBullet = false;
 			this.gameObject.GetComponent<Transform> ().position = GameObject.Find ("BulletSpawn").GetComponent<Transform> ().position;
 			liveBulletCount = 0;
-			enemy.gameObject.GetComponentInChildren<Material>().color = Color.red ;
-			Debug.Log ("called");
 		}
-
-
 	}
 
 	// Update is called once per frame
@@ -55,23 +55,43 @@ public class Bullet : MonoBehaviour {
 		firingPosition = new Vector3((int)firingPosition.x, 0, (int)firingPosition.z);
 		targetLine = GameObject.Find ("GunTarget").GetComponent<Transform> ().position;
 		targetLine = new Vector3((int)targetLine.x, 0, (int)targetLine.z);
+//		if (activeBullet == true) 
+//		{
+//			bltVelo = CalculateDistanceandSpeed (targetLine, firingPosition);
+//
+//			this.gameObject.GetComponent<Rigidbody> ().AddForce((bltVelo * 100), ForceMode.Impulse);
+//			liveBulletCount++;
+//
+//			if(liveBulletCount == 100)
+//			{
+//				this.gameObject.GetComponent<Rigidbody> ().Sleep ();
+//				activeBullet = false;
+//				this.gameObject.GetComponent<Transform> ().position = GameObject.Find ("BulletSpawn").GetComponent<BulletPooling> ().ReturnBullet();
+//				liveBulletCount = 0;
+//			}
+//		}
+
+	}
+
+	void FixedUpdate()
+	{
 		if (activeBullet == true) 
 		{
-			bulletStartTime = Time.time;
+			bulletRend.enabled = true;
 			bltVelo = CalculateDistanceandSpeed (targetLine, firingPosition);
 
-			this.gameObject.GetComponent<Rigidbody> ().AddForce((bltVelo * 100), ForceMode.Impulse);
-
-			//bltVelo += bulletTvlDist * Time.deltaTime;
+			this.gameObject.GetComponent<Rigidbody> ().AddForce((bltVelo * 500), ForceMode.Impulse);
 			liveBulletCount++;
+
 			if(liveBulletCount == 100)
 			{
+				bulletRend.enabled = false;
 				this.gameObject.GetComponent<Rigidbody> ().Sleep ();
 				activeBullet = false;
 				this.gameObject.GetComponent<Transform> ().position = GameObject.Find ("BulletSpawn").GetComponent<BulletPooling> ().ReturnBullet();
 				liveBulletCount = 0;
+
 			}
 		}
-
 	}
 }
